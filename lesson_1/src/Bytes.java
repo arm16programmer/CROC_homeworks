f18dcb3e3034ac6a8e32db4f77d61bf071a7ba18
jesssica-khloyan <hloandzessika087@gmail.com>
2023-03-15 14:56:34 +0300

    first commit

---
+++ lesson_1/src/Bytes.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Bytes {
+
+    public static void main(String[] args) {
+
+        double bytes;
+        String[] measureUnit = new String[] {"B", "KB", "MB", "GB", "TB"};
+        System.out.print("Введите число байтов: ");
+        Scanner sc = new Scanner(System.in);
+        bytes = sc.nextDouble();
+
+        int count = 0;
+        while (bytes >= 1023.9) {
+            bytes = bytes/1024;
+            count++;
+        }
+
+        if (count<5) {
+            System.out.println(String.format(Locale.ROOT,"%.1f", bytes) + " " + measureUnit[count]);        } else {
+            System.out.println("Слишком большое число, введите меньшее значение!");
+        }
+    }
+}
